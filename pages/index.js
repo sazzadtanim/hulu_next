e8c7673248c4ac8dfb1522a391da64c250a0543b
sazzadtanim <tanimsazzad@gmail.com>
2022-11-26 10:24:06 +0600

    FEATURED AND TOP  MOVIES ADDED

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Nav from "../components/Nav";
 import Content from "../components/Content";
 import Pagination from "../components/Pagination";
 
-export default function Home({ movies }) {
+export default function Home({ movies, featuredMovies, topMovies }) {
   return (
     <div>
       <Head>
@@ -18,6 +18,18 @@ export default function Home({ movies }) {
       <Nav />
       {/* Main Content */}
       <Content movies={movies} />
+      {/* Featured Movies */}
+      <h1 className="text-yellow-400 text-6xl my-10 flex justify-center text-bold">
+        Featured today
+      </h1>
+      <Content movies={featuredMovies} />
+
+      {/* Top movies */}
+      <h1 className="text-yellow-400 text-6xl my-10 flex justify-center text-bold">
+        Top Movies
+      </h1>
+
+      <Content movies={topMovies} />
       {/* pagination */}
       <Pagination />
     </div>
@@ -25,16 +37,30 @@ export default function Home({ movies }) {
 }
 
 export async function getServerSideProps(context) {
-  console.log(context);
   const resp = await fetch(
     "https://api.themoviedb.org/3/discover/movie?api_key=9c3ef9cbaa77b72892d9c3b4c8b66fb7&with_genres=12"
   );
   const s = await resp.json();
   const movies = s.results;
 
+  const featuredMoviesResp = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=9c3ef9cbaa77b72892d9c3b4c8b66fb7&with_genres=28"
+  );
+  const featuredMoviesRespJson = await featuredMoviesResp.json();
+
+  const featuredMovies = featuredMoviesRespJson.results;
+
+  const topMoviesResp = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=9c3ef9cbaa77b72892d9c3b4c8b66fb7&with_genres=10749"
+  );
+  const topMoviesRespJson = await topMoviesResp.json();
+
+  const topMovies = topMoviesRespJson.results;
   return {
     props: {
       movies,
+      featuredMovies,
+      topMovies,
     },
   };
 }
